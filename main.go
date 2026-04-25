c78e9d61d9539c65a6071ad7a37b9c71e5119e9d
koustreak <koushik.dutta.py@protonmail.com>
2026-04-25 19:39:41 +0530

    refactor: migrate frontend dependencies and node_modules to the ui directory

--- main.go
+++ main.go
@@ -1,11 +1,20 @@
 package main
 
 import (
+	"embed"
 	"log"
 
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
+	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+	"github.com/wailsapp/wails/v2/pkg/options/linux"
 )
+
+//go:embed all:ui/dist
+var assets embed.FS
+
+//go:embed build/appicon.png
+var icon []byte
 
 func main() {
 	err := wails.Run(&options.App{
@@ -14,6 +23,13 @@ func main() {
 		Height:    768,
 		MinWidth:  800,
 		MinHeight: 600,
+		AssetServer: &assetserver.Options{
+			Assets: assets,
+		},
+		Linux: &linux.Options{
+			Icon:        icon,
+			ProgramName: "deloc",
+		},
 	})
 	if err != nil {
 		log.Fatal(err)
